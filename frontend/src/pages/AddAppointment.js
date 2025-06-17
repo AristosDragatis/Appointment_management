1c5cbc093d7e4b22831f4ddda279d8c3f63a0254
Aristeidis Dragatis <dragatis.aris@gmail.com>
2025-06-17 22:20:41 +0300

    add appointment updated

--- frontend/src/pages/AddAppointment.js
+++ frontend/src/pages/AddAppointment.js
@@ -1,17 +1,32 @@
+import { Link } from 'react-router-dom';
 export default function AddAppointment() {
   return (
     <div className="text-center">
       <h1>Add New Appointment</h1>
       <form>
         <label for="username">Firstname:</label><br></br>
-        <input type="text" id="firstname" name="username"></input><br></br>
+        <input type="text" id="firstname" name="username" placeholder='e.g. John'></input><br></br>
         <label for="lastname">Lastname:</label><br></br>
-        <input type="text" id="lastname" name="lastname"></input><br></br>
-        <label for="profession">Profession:</label><br></br>
+        <input type="text" id="lastname" name="lastname" placeholder='e.g. Smith'></input><br></br>
+        <label htmlFor="email">Email:</label><br/>
+        <div className="d-flex justify-content-center">
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            className="w-25 form-control"
+            placeholder='someone@example.com'
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            required 
+          />
+        </div>
+        <br></br>
+        <label for="profession">Service Name:</label><br></br>
         <div className="d-flex justify-content-center">
         <select class="form-select form-select-sm w-25 text-center" aria-label=".form-select-sm example">
             <option selected>--Select One--</option>
-            <option value="1">Barber</option>
+            <option value="1">Haircut</option>
+            <option value="2">Haircut/Beard</option>
         </select>
         </div>
 
@@ -28,6 +43,8 @@ export default function AddAppointment() {
         <br></br> 
         <button type="button" class="btn btn-success">Submit</button>
       </form>
+      <br></br>
+      <Link to="/users" className="btn btn-secondary btn-sm">Back</Link>
     </div>
   );
 }
