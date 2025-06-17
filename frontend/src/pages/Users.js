f2e473d364f5910fd0273e268711d53058a7564e
Aristeidis Dragatis <dragatis.aris@gmail.com>
2025-06-17 21:02:25 +0300

    Made the text-center change

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Users() {
   return (
-    <div>
+    <div className='text-center'>
       <h2>User Management</h2>
       <Link to="/addappointment" className="btn btn-primary mb-2">Add Appointment</Link>
       <br/>
