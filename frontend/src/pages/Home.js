871b6ea8d05309522e75d8d58c1b8a2a7f5349ef
Aristeidis Dragatis <dragatis.aris@gmail.com>
2025-06-17 21:01:46 +0300

    Changed the name of the button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="text-center">
-      <h1>Welcome To Appointment Manager</h1>
-      <Link to="/users" className="btn btn-primary">Users</Link>
+      <h2>Welcome To Appointment Manager</h2>
+      <Link to="/users" className="btn btn-primary">Open</Link>
     </div>
   );
 }
